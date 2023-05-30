474e9a8b54d1063bfb5d9b0f5c5616d8b89064fa
Suzanne <suzanne.elize.roberts@gmail.com>
2023-05-30 16:05:18 +0200

    Split the big confusing comparison graph into single separate graphs... to later be combined into logical groups

--- designParameterSensitivity.py
+++ designParameterSensitivity.py
@@ -9,50 +9,84 @@ from emlc import levSim
 
 def designParameterSensitivity():
 
+### Comparing different coil designs, all with a
+### copper sample and
+### argon atmosphere; and 
+### using the same 15x15x15 discretisation in all cases.
+
+
+# Instantiating a sample and an atmosphere
+    mySample     = dd.Cu
+    myAtmosphere = dd.Ar
+
+# Discretizing the sample
     Layers   = 15
     Sections = 15
     Slices   = 15
     
-    myCoil       = dd.sz # dd.loop2dir1
-    mySample     = dd.Cu
-    myAtmosphere = dd.Ar
     
-    tupOut = levSim(myCoil, mySample, myAtmosphere, Layers, Sections, Slices)
+# Instantiating and plotting the different coil designs
+
+    # Szekely paper coil design
+    coilSz      = dd.sz # dd.loop2dir1
+    tupOutSz    = levSim(coilSz, mySample, myAtmosphere, Layers, Sections, Slices)
+    pl.plot(tupOutSz[0], tupOutSz[1], '-bo')
+    pl.plot(tupOutSz[2], tupOutSz[3], 'rx')
+    pl.plot(tupOutSz[0], tupOutSz[4]*np.ones(len(tupOutSz[0])), 'k')
+    
+#    myCoil.I = 130
+#    dd.ro.Ivec = (myCoil.loops[:,4]*myCoil.I) + (np.abs(myCoil.loops[:,4]-1)*(-myCoil.I))
+    
+#    tupOut = (posVec, forceVec, aSamplePos, F_lift_z, sampleWeight, powerVec, tempVec, T, simTime)
+    
+    print('powerVec', tupOutSz[5])
+    print('tempVec', tupOutSz[6])
+    print('T', tupOutSz[7])
+    print('simTime', tupOutSz[8])
+    
 
     myCoil2 = dd.sz_doubleLoops
-    mySample2 = dd.Cu
-    myAtmosphere2 = dd.Ar
-    
-    tupOut2 = levSim(myCoil2, mySample2, myAtmosphere2, Layers, Sections, Slices)
+    tupOut2 = levSim(myCoil2, mySample, myAtmosphere, Layers, Sections, Slices)
+    pl.plot(tupOut2[0], tupOut2[1], '-go')
+    pl.plot(tupOut2[2], tupOut2[3], 'cx')
+    pl.plot(tupOut2[0], tupOut2[4]*np.ones(len(tupOut2[0])), 'm')
     
     myCoil3 = dd.sz_doubleBottomLoops
-    mySample3 = dd.Cu
-    myAtmosphere3 = dd.Ar
-    
-    tupOut3 = levSim(myCoil3, mySample3, myAtmosphere3, Layers, Sections, Slices)
+    tupOut3 = levSim(myCoil3, mySample, myAtmosphere, Layers, Sections, Slices)
+    pl.plot(tupOut3[0], tupOut3[1], '--go')
+    pl.plot(tupOut3[2], tupOut3[3], 'cx')
+    pl.plot(tupOut3[0], tupOut3[4]*np.ones(len(tupOut3[0])), 'm')
     
     myCoil4 = dd.sz_8b2t
-    mySample4 = dd.Cu
-    myAtmosphere4 = dd.Ar
     print(myCoil4.I)
-    tupOut4 = levSim(myCoil4, mySample4, myAtmosphere4, Layers, Sections, Slices)
+    tupOut4 = levSim(myCoil4, mySample, myAtmosphere, Layers, Sections, Slices)
     print(tupOut4[7])
+    pl.plot(tupOut4[0], tupOut4[1], '-bs')
+    pl.plot(tupOut4[2], tupOut4[3], 'cx')
+    pl.plot(tupOut4[0], tupOut4[4]*np.ones(len(tupOut4[0])), 'm')
     
     myCoil4.I = 200.0
     print(myCoil4.I)
-    tupOut5 = levSim(myCoil4, mySample4, myAtmosphere4, Layers, Sections, Slices)
+    tupOut5 = levSim(myCoil4, mySample, myAtmosphere, Layers, Sections, Slices)
+    pl.plot(tupOut5[0], tupOut5[1], '--mo')
+    pl.plot(tupOut5[2], tupOut5[3], 'cx')
+    pl.plot(tupOut5[0], tupOut5[4]*np.ones(len(tupOut5[0])), 'm')
     
     myCoil4.I = 400.0
     print(myCoil4.I)
     myCoil4.f = 450.0
     print(myCoil4.f)
-    tupOut6 = levSim(myCoil4, mySample4, myAtmosphere4, Layers, Sections, Slices)
+    tupOut6 = levSim(myCoil4, mySample, myAtmosphere, Layers, Sections, Slices)
     print(tupOut6[7])
+    pl.plot(tupOut6[0], tupOut6[1], '--cx')
+    pl.plot(tupOut6[2], tupOut6[3], 'cx')
+    pl.plot(tupOut6[0], tupOut6[4]*np.ones(len(tupOut5[0])), 'm')
     
     myCoil7 = dd.sr_1b
-    mySample7 = dd.Cu
-    myAtmosphere7 = dd.Ar
-    tupOut7 = levSim(myCoil7, mySample7, myAtmosphere7, Layers, Sections, Slices)
+    tupOut7 = levSim(myCoil7, mySample, myAtmosphere, Layers, Sections, Slices)
+    pl.plot(tupOut7[0], tupOut7[1], '--bx')
+    pl.plot(tupOut7[2], tupOut7[3], 'rx')
+    pl.plot(tupOut7[0], tupOut7[4]*np.ones(len(tupOut7[0])), 'k')
 
 # tube radius
 # size of the gap between the different current direction loops
@@ -65,38 +99,6 @@ def designParameterSensitivity():
 # (the "and" case will be maximizing the intergral under the Fz - weight curve)
    
 
-#    myCoil.I = 130
-#    dd.ro.Ivec = (myCoil.loops[:,4]*myCoil.I) + (np.abs(myCoil.loops[:,4]-1)*(-myCoil.I))
-    
-#    tupOut = (posVec, forceVec, aSamplePos, F_lift_z, sampleWeight, powerVec, tempVec, T, simTime)
-    
-    print('powerVec', tupOut[5])
-    print('tempVec', tupOut[6])
-    print('T', tupOut[7])
-    print('simTime', tupOut[8])
-    
-    pl.figure()
-    pl.plot(tupOut[0], tupOut[1], '-bo')
-    pl.plot(tupOut[2], tupOut[3], 'rx')
-    pl.plot(tupOut[0], tupOut[4]*np.ones(len(tupOut[0])), 'k')
-    pl.plot(tupOut2[0], tupOut2[1], '-go')
-    pl.plot(tupOut2[2], tupOut2[3], 'cx')
-    pl.plot(tupOut2[0], tupOut2[4]*np.ones(len(tupOut2[0])), 'm')
-    pl.plot(tupOut3[0], tupOut3[1], '--go')
-    pl.plot(tupOut3[2], tupOut3[3], 'cx')
-    pl.plot(tupOut3[0], tupOut3[4]*np.ones(len(tupOut3[0])), 'm')
-    pl.plot(tupOut4[0], tupOut4[1], '-bs')
-    pl.plot(tupOut4[2], tupOut4[3], 'cx')
-    pl.plot(tupOut4[0], tupOut4[4]*np.ones(len(tupOut4[0])), 'm')
-    pl.plot(tupOut5[0], tupOut5[1], '--mo')
-    pl.plot(tupOut5[2], tupOut5[3], 'cx')
-    pl.plot(tupOut5[0], tupOut5[4]*np.ones(len(tupOut5[0])), 'm')
-    pl.plot(tupOut6[0], tupOut6[1], '--cx')
-    pl.plot(tupOut6[2], tupOut6[3], 'cx')
-    pl.plot(tupOut6[0], tupOut6[4]*np.ones(len(tupOut5[0])), 'm')
-    pl.plot(tupOut7[0], tupOut7[1], '--bx')
-    pl.plot(tupOut7[2], tupOut7[3], 'rx')
-    pl.plot(tupOut7[0], tupOut7[4]*np.ones(len(tupOut7[0])), 'k')
     pl.show()
     
     
